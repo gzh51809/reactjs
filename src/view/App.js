556fa366325ebb580c0786411285253659801fa7
xuyang <13826016185@163.com>
2019-02-06 22:39:06 +0800

    header 和 footer

--- src/view/App.js
+++ src/view/App.js
@@ -1,12 +1,16 @@
 import React, {Component} from 'react';
 import RounterIndex from "../router/router";
+import Header from "./Header";
+import Footer from "./Footer";
 
 class App extends Component {
   render() {
     return (
       <div>
       <main className="main">
+              <Header  />
                     <RounterIndex />
+              <Footer />
                </main>
       </div>
     );
